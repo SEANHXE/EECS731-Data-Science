d37ff0b317c9f5aed200094b345f9d20a1948c4c
SEANHXE <32432804+SEANHXE@users.noreply.github.com>
2019-09-16 00:17:43 -0500

    Add files via upload

---
+++ Assignment1/code.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+Rawdata = pd.read_csv('F:\Jupyter Notebook\Jimmy Wrangler, Data Explorer\crime1.csv', encoding='latin',low_memory=False)
+print(Rawdata.head())
